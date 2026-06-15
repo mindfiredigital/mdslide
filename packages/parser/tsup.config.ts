189076b4fb776706a75af8dc20e57b7ab49538ef
sobhan-s <sobhans@mindfiresolutions.com>
2026-06-15 23:30:12 +0530

    chore(config): add setup for the parser package

---
+++ packages/parser/tsup.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'tsup';
+import { baseConfig } from '../../tsup.config.base';
+
+export default defineConfig({
+  ...baseConfig,
+  entry: ['src/index.ts', 'src/cli.ts'],
+  external: ['remark-gfm', 'remark-parse', 'unified'],
+});
